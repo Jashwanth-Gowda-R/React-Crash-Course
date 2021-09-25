6861614c907a44f987e70c244be98ff5df099055
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-09-25 19:08:27 +0530

    event handler

--- react_course/src/components/todo.js
+++ react_course/src/components/todo.js
@@ -1,9 +1,12 @@
 function Todo(props) {
+  const onDeleteHandler = () => {};
   return (
     <div className="card">
       <h2>{props.title}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={onDeleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
