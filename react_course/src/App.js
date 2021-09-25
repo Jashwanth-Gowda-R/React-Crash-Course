960a8dc06d98f55e8a81801e8f47aca5b64a166d
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-09-25 19:16:39 +0530

    added more  components

--- react_course/src/App.js
+++ react_course/src/App.js
@@ -1,3 +1,5 @@
+import Backdrop from "./components/Backdrop";
+import Modal from "./components/Modal";
 import Todo from "./components/todo";
 
 function App() {
@@ -6,6 +8,8 @@ function App() {
       <h1>My Todos</h1>
       <Todo title="react" />
       <Todo title="flutter" />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
