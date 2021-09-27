966bf3c79ff9b348eaf3b081f1ba653abd4252e9
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-09-27 18:38:22 +0530

    added routing to the project

--- meetup_demo_app/src/App.js
+++ meetup_demo_app/src/App.js
@@ -1,5 +1,24 @@
+import { Route, Switch } from "react-router";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/favourite">
+          <FavoritesPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
