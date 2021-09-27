9fbb96af17abe1cd74aa8ed15ff12c62eb613d7a
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-09-27 18:49:49 +0530

    added navigation

---
+++ meetup_demo_app/src/components/layout/MainNavigation.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+function MainNavigation() {
+  return (
+    <header>
+      <div>Shani Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/favourite">My Favorites</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">Add New Meetup</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
