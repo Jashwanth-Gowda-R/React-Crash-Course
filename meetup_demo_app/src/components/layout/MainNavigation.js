b5382689357600089c927ce480897a7e83370547
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-10-05 14:42:48 +0530

    added styles

--- meetup_demo_app/src/components/layout/MainNavigation.js
+++ meetup_demo_app/src/components/layout/MainNavigation.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
   return (
-    <header>
-      <div>Shani Meetups</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>Shani Meetups</div>
       <nav>
         <ul>
           <li>
